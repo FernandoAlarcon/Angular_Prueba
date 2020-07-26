891afbc965aafbbc65c3284d3af1a2a584fa60fa
Fernando Alarcon <erickfernando_20@hotmail.com>
2020-07-26 12:59:12 -0500

    Domingo

--- src/app/component/firs.component.ts
+++ src/app/component/firs.component.ts
@@ -15,5 +15,35 @@ export class HelloWorld {
                 'Germit',
                 'bruce'
             ];
-    activated = true;
+    activated: boolean = false;
+
+    // tslint:disable-next-line: no-inferrable-types
+    name: string = 'Fernando Alarcon';
+  
+    adress: {
+        street:string;
+        city:string;
+    };
+
+    hobbies: string[];
+    age: number;
+    
+    womens: string[] = [
+        'Melissa',
+        'Keren',
+        'Andrea',
+        'Nesly',
+        'Dayana'
+    ];
+
+    constructor(){
+        this.age = 28;
+        this.adress = {
+            street : '221B Baker Street',
+            city: 'London',
+        // tslint:disable-next-line: no-trailing-whitespace
+        }; 
+
+        this.hobbies = ['swimin', 'read', 'write'];
+    }
 }
